053430f6c5c0c9d42d0beeb294b8b423ddfc1fc3
jasonbejoyk-hub <jason.bejoy.k@gmail.com>
2026-07-01 22:10:23 +0530

    Completed data cleaning and prepared dataset

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import os
-
 print(os.path.exists("data/raw/India_Emissions.filtered/co-emissions-per-capita.csv"))
 
 df = pd.read_csv(
     "data/raw/India_Emissions.filtered/co-emissions-per-capita.csv"
 )
-
+#Loading Data Frame
 print(df.head())
 
 print(df.tail())
@@ -16,5 +15,23 @@ print(df.head(10))
 print(df.info())
 
 print(df.columns)
+
+#Cleaning Data Frame
+print("\nMissing values:")
+
+print(df.isnull().sum())
+
+print("\nDuplicate rows:")
+
+print(df.duplicated().sum())
+
+clean_df = df[["Year", "CO₂ emissions per capita"]]
+
+print(clean_df.head())
+print(clean_df.tail())
+
+
+
+
 
 
